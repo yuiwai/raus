51d034271187a91658399fc99d86ddfe38973f90
yuiwai <yuichiro.iwai@gmail.com>
2018-05-31 09:48:23 +0900

    LocalStorageによる永続化実装中

--- core/src/main/scala/com/yuiwai/raus/infrastructure/Persistence.scala
+++ core/src/main/scala/com/yuiwai/raus/infrastructure/Persistence.scala
@@ -1,4 +1,9 @@
 package com.yuiwai.raus.infrastructure
 
 trait Persistence
-
+sealed trait PersistentStrategy
+object PersistentStrategies {
+  case object AllInOneSerialization extends PersistentStrategy
+  case object Relational extends PersistentStrategy
+}
+trait PersistentStorage
