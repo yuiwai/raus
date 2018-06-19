81063e2ac9546fc5e0a0125c444f4587ba100d01
yuiwai <yuichiro.iwai@gmail.com>
2018-06-20 06:58:59 +0900

    ReactJS version.

--- react/src/main/scala/com/yuiwai/raus/react/RausComponent.scala
+++ react/src/main/scala/com/yuiwai/raus/react/RausComponent.scala
@@ -10,15 +10,36 @@ import scala.scalajs.js.annotation.{JSExport, JSExportTopLevel}
 @JSExportTopLevel("com.yuiwai.RausComponent")
 object RausComponent {
   case class State(raus: Raus, key: String) {
-    def load = {
+    def load(): State = {
       raus.load(key)
+      this
+    }
+    def save(): State = {
+      raus.save(key)
+      this
+    }
+    def addTask(): State = {
+      raus.addTask("test")
       this
     }
   }
   class Backend(bs: BackendScope[Unit, State]) {
-    def load = bs.modState(_.load)
+    def load() = bs.modState(_.load())
+    def save() = bs.modState(_.save())
+    def addTask() = bs.modState(_.addTask())
     def render(s: State) =
       <.div(
+        <.div(
+          <.input(),
+          <.button(
+            ^.onClick --> addTask(),
+            "add"
+          ),
+          <.button(
+            ^.onClick --> save(),
+            "save"
+          )
+        ),
         s.raus.tasks().map(task => <.div(task.title)).toTagMod
       )
   }
@@ -29,6 +50,6 @@ object RausComponent {
     ScalaComponent.builder[Unit]("RausComponent")
       .initialState(State(raus, key))
       .renderBackend[Backend]
-      .componentDidMount(_.backend.load)
+      .componentDidMount(_.backend.load())
       .build
 }
