fa47fccf1b1714dd5ff529ced8d66d49b8001ca0
yuiwai <yuichiro.iwai@gmail.com>
2018-06-24 02:06:17 +0900

    Mapper経由での処理実装

--- core/src/main/scala/com/yuiwai/raus/infrastructure/Mapper.scala
+++ core/src/main/scala/com/yuiwai/raus/infrastructure/Mapper.scala
@@ -1,9 +1,19 @@
 package com.yuiwai.raus.infrastructure
 
-import com.yuiwai.raus.model.User
+import java.util.UUID
+
+import com.yuiwai.raus.model.{Saved, Task, TaskState, User}
 
 trait Mapper {
-  def toMap(user: User): Map[String, Any] = ???
-  def fromMap(map: Map[String, Any]): User = ???
+  private val Tasks = "tasks"
+  def toMap(user: User): Map[String, Map[String, String]] = Map(
+    Tasks -> user.tasks.map { case (uuid, task) => uuid.toString -> task.title }
+  )
+  def fromMap(map: Map[String, Map[String, String]]): User = map.get(Tasks)
+    .map { xs =>
+      User().copy(tasks = xs.map {
+        case (id, title) => UUID.fromString(id) -> Task(UUID.fromString(id), TaskState(title), Saved)
+      })
+    } getOrElse User()
 }
 object Mapper extends Mapper
--- ext/jvm/src/main/scala/com/yuiwai/raus/ext/UPickleSerializer.scala
+++ ext/jvm/src/main/scala/com/yuiwai/raus/ext/UPickleSerializer.scala
@@ -1,10 +1,14 @@
 package com.yuiwai.raus.ext
 
-import com.yuiwai.raus.infrastructure.Serializer
+import com.yuiwai.raus.infrastructure.{Mapper, Serializer}
 import com.yuiwai.raus.model._
 import upickle.default._
 
 trait UPickleSerializer extends Serializer {
-  override protected def serialize(user: User): String = ???
-  override protected def deserialize(data: String): User = ???
+  override protected def serialize(user: User): String = {
+    write(Mapper.toMap(user))
+  }
+  override protected def deserialize(data: String): User = {
+    Mapper.fromMap(read[Map[String, Map[String, String]]](data))
+  }
 }
