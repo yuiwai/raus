71be0a66600454cb1452134a5670b4582fe35457
Yuichiro Iwai <yuichiro.iwai@gmail.com>
2018-07-20 23:46:41 +0900

    update raus react component.

--- react/src/main/scala/com/yuiwai/raus/RausComponent.scala
+++ react/src/main/scala/com/yuiwai/raus/RausComponent.scala
@@ -1,14 +1,25 @@
-package com.yuiwai.raus.react
+package com.yuiwai.raus
 
 import com.yuiwai.raus.ext.Raus
 import japgolly.scalajs.react._
 import japgolly.scalajs.react.vdom.html_<^._
 import org.scalajs.dom
+import org.scalajs.dom.raw.HTMLInputElement
 
 import scala.scalajs.js.annotation.{JSExport, JSExportTopLevel}
 
-@JSExportTopLevel("com.yuiwai.RausComponent")
+@JSExportTopLevel("raus.RausComponent")
 object RausComponent {
+  @JSExport("apply")
+  def apply(id: String, raus: Raus, key: String) = component(raus, key)()
+    .renderIntoDOM(dom.document.getElementById(id))
+  def component(raus: Raus, key: String) =
+    ScalaComponent.builder[Unit]("RausComponent")
+      .initialState(State(raus, key))
+      .renderBackend[Backend]
+      .componentDidMount(_.backend.load())
+      .build
+
   case class State(raus: Raus, key: String) {
     def load(): State = {
       raus.load(key)
@@ -18,19 +29,23 @@ object RausComponent {
       raus.save(key)
       this
     }
-    def addTask(): State = {
-      raus.addTask("test")
+    def addTask(title: String): State = {
+      raus.addTask(title)
       this
     }
   }
-  class Backend(bs: BackendScope[Unit, State]) {
+
+  final class Backend(bs: BackendScope[Unit, State]) {
+    private val taskRef = Ref[HTMLInputElement]
     def load() = bs.modState(_.load())
     def save() = bs.modState(_.save())
-    def addTask() = bs.modState(_.addTask())
+    def addTask() = taskRef.foreachCB { i =>
+      bs.modState(_.addTask(i.value))
+    }
     def render(s: State) =
       <.div(
         <.div(
-          <.input(),
+          <.input.withRef(taskRef)(),
           <.button(
             ^.onClick --> addTask(),
             "add"
@@ -43,13 +58,4 @@ object RausComponent {
         s.raus.tasks().map(task => <.div(task.title)).toTagMod
       )
   }
-  @JSExport("apply")
-  def apply(id: String, raus: Raus, key: String) =
-    component(raus, key)().renderIntoDOM(dom.document.getElementById(id))
-  def component(raus: Raus, key: String) =
-    ScalaComponent.builder[Unit]("RausComponent")
-      .initialState(State(raus, key))
-      .renderBackend[Backend]
-      .componentDidMount(_.backend.load())
-      .build
 }
