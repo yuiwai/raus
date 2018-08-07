d1022e66cc601e7c3b4644e44c9b92a0f37f8c0b
Yuichiro Iwai <yuichiro.iwai@gmail.com>
2018-08-08 08:17:35 +0900

    Color周り、模索

--- ncurses/src/main/scala/com/yuiwai/raus/ncurses/Sample.scala
+++ ncurses/src/main/scala/com/yuiwai/raus/ncurses/Sample.scala
@@ -8,16 +8,19 @@ object Sample {
 
     val screen: Ptr[Window] = initscr()
     val win = newwin(10, 10, 1, 1)
+    val pair1 = 1.toShort
     start_color()
+    init_pair(pair1, Colors.COLOR_BLACK, Colors.COLOR_RED)
+    attron(COLOR_PAIR(pair1))
     cbreak()
     noecho()
     loop()
+    attroff(COLOR_PAIR(pair1))
     endwin()
   }
   def loop(): Unit = {
     val char = getch
-    print(char)
-    print(" ")
+    mvprintw(10, 10, c"%d", char)
     loop()
   }
 }
@@ -29,8 +32,26 @@ object Ncurses {
   def initscr(): Ptr[Window] = extern
   def newwin(nlines: CInt, ncols: CInt, beginY: CInt, beginX: CInt): Ptr[Window] = extern
   def endwin(): CInt = extern
+  def has_colors(): CBool = extern
   def start_color(): CInt = extern
+  def init_pair(pair: CShort, f: CShort, b: CShort): CInt = extern
+  def COLOR_PAIR(pair: CShort): CInt = extern
+  def attron(attribute: CInt): CInt = extern
+  def attroff(attribute: CInt): CInt = extern
   def cbreak(): CInt = extern
   def noecho(): CInt = extern
   def getch: CInt = extern
+  def mvprintw(y: CInt, x: CInt, fmt: CString, args: CVararg*): CInt = extern
+  def mvaddch(y: CInt, x: CInt, ch: CChar): CInt = extern
+}
+
+object Colors {
+  val COLOR_BLACK: CShort = 0.toShort
+  val COLOR_RED: CShort = 1.toShort
+  val COLOR_GREEN: CShort = 2.toShort
+  val COLOR_YELLOW: CShort = 3.toShort
+  val COLOR_BLUE: CShort = 4.toShort
+  val COLOR_MAGENTA: CShort = 5.toShort
+  val COLOR_CYAN: CShort = 6.toShort
+  val COLOR_WHITE: CShort = 7.toShort
 }
